8b51100b8c7e4a6a0fc5dc9345b3abce078235fc
kokonutzlabs <kortneyd24@gmail.com>
2025-11-18 07:35:18 -0500

    Update form handling and stylesheet path in nook-book.php

--- nook-book.php
+++ nook-book.php
@@ -2,23 +2,23 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
-        <link rel="stylesheet" href="nook-book-layout.css">
+        <link rel="stylesheet" href="Project1/nook-book-layout.css">
         <title>nook book voting, reacting, and commenting</title>
     </head>
     
     <body>
         <?php
-            if($_SERVER['REQUEST_METHOD'] === 'POST') {  # checks if someone added data, if so it proceeds to the next three functions
-                if (isset($_POST['best-fit'])) {  // if the data came from the best-fit form it continues to the next line
-                    $bestfit = htmlspecialchars($_POST['best-fit']); // (safely saves variables each option) turns choice into a variable for echo
-                    echo "You voted for: $bestfit<br>"; // message you get when everything checks out
+            if ($_SERVER['REQUEST_METHOD'] === 'POST') {  //checks if someone added data, if so it proceeds to the next three functions
+                if (isset($_POST['best_fit'])) { //if the data came from the best-fit form it continues to the next line
+                    $bestfit = ($_POST['best_fit']); //(safely saves variables each option) turns choice into a variable for echo
+                    echo "You voted for: $bestfit<br>"; //message you get when everything checks out
                 }
-                if(isset($_POST['reaction'])) {
-                    $reaction = htmlspecialchars($_POST['reaction']);
+                if (isset($_POST['reaction'])) {
+                    $reaction = ($_POST['reaction']);
                     echo "You reacted with: $reaction<br>";
                 }
-                if(isset($_POST['comment'])) {
-                    $comment = htmlspecialchars($_POST['comment']);
+                if (isset($_POST['comment'])) {
+                    $comment = ($_POST['comment']);
                     echo "You commented: $comment<br>";
                 }
             }
